327a35ac9b5ebdc15761a6d579193b4237640e65
Larry Landon <larryl011010@gmail.com>
2018-02-03 11:50:06 -0600

    update basic client to import checks/contactGroups to NP

--- clients/nodePingClient.js
+++ clients/nodePingClient.js
@@ -1,9 +1,40 @@
 const rp = require('request-promise');
+const Promise = require('bluebird');
 
 const apiBaseUrl = 'https://api.nodeping.com/api/1/';
 
+
 module.exports = {
-  sync: function(ingressData) {
-    console.log(ingressData);
+  syncContactGroups: function(contactGroups, credentials) {
+    Promise.map(contactGroups, (contactGroup) => {
+      var options = {
+        method: 'POST',
+        uri: `${apiBaseUrl}contactgroups/?token=${credentials.token}`,
+        body: {
+          name: contactGroup.GroupName,
+          members: contactGroup.members
+        },
+        json: true
+      };
+      return rp(options);
+    })
+    .then((results) => {
+      console.log(results);
+    })
+  },
+  syncTests: function(tests, credentials) {
+    Promise.map(tests, (test) => {
+      console.log(test)
+      var options = {
+        method: 'POST',
+        uri: `${apiBaseUrl}checks/?token=${credentials.token}`,
+        body: test,
+        json: true
+      };
+      return rp(options)
+    })
+    .then((results) => {
+      console.log(results);
+    });
   }
 }
