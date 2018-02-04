28e55f9a75e34b3a46e14c1de8b647629784938a
Larry Landon <larryl011010@gmail.com>
2018-02-04 13:40:33 -0600

    add functions to map foreign contacts to existing NP contacts, create contact

--- clients/NP_utils.js
+++ clients/NP_utils.js
@@ -1,17 +1,40 @@
 const rp = require('request-promise');
-const Primise = require('bluebird');
+const Promise = require('bluebird');
 const credentials = require('../credentials.js');
+const _ = require('lodash');
 const apiBaseUrl = 'https://api.nodeping.com/api/1/';
 
 npCredentials = credentials.nodePing;
+var options = {
+  method: 'GET',
+  uri: `${apiBaseUrl}contacts/?token=${npCredentials.token}`,
+  json: true
+};
 
 module.exports = {
   getNpContacts: function() {
-    var options = {
-      method: 'GET',
-      uri: `${apiBaseUrl}contacts/?token=${npCredentials.token}`,
-      json: true
-    };
     return rp(options)
+  },
+  //map foreign contacts to existing NP contacts, match on email
+  mapContacts: function(NpContacts, foreignContacts) {
+    return Promise.map(foreignContacts, (foreignContact) => {
+      contact = _.cloneDeep(foreignContact);
+
+      for (NpContactID in NpContacts) {
+        NpContact = NpContacts[NpContactID]
+        if (NpContact.name === foreignContact.contactAddress){
+          contact.NpContact = NpContact;
+        }
+      }
+      return contact
+    })
+  },
+  createContact: function(contactInfo) {
+    options.method = 'PUT'
+    options.body = contactInfo
+    return rp(options)
+    .then((results) => {
+      console.log(results)
+    })
   }
 }
