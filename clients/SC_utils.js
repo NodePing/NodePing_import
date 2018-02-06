276c29e341a60c11cc3607fc154f48ed04b23d8e
Larry Landon <larryl011010@gmail.com>
2018-02-05 20:05:49 -0600

    refactor test->check mapping

--- clients/SC_utils.js
+++ clients/SC_utils.js
@@ -1,22 +1,27 @@
-const _ = require('lodash');
+const _ = require('lodash')
 
 module.exports = {
   //map statusCake check to NodePing test
-  mapTestToCheck: function (test) {
-    const check = {
-      type: test.TestType,
-      label: test.WebsiteName,
-      target: test.WebsiteURL,
-      enabled: !test.Paused,
-      public: test.Public,
-      interval: test.CheckRate / 60
-    };
-    return check;
+  mapTestsToChecks: function (tests) {
+    const checks = []
+    tests.forEach((test) => {
+      let check = {
+        type: test.TestType,
+        label: test.WebsiteName,
+        target: test.WebsiteURL,
+        enabled: !test.Paused,
+        public: test.Public,
+        interval: test.CheckRate / 60,
+        foreignContactIDs: [test.ContactGroup]
+      }
+      checks.push(check)
+    })
+    return checks
   },
   //remap statusCake contactGroups into array of contacts
   mapContactsAndGroups: function (contactGroups) {
-    var contacts = [];
-    var emails = [];
+    var contacts = []
+    var emails = []
     contactGroups.forEach((contactGroup) => {
       emails = _.union(emails, contactGroup.Emails)
 
@@ -27,8 +32,8 @@ module.exports = {
         contactType: 'email',
         foreignContactGroups: []
       }
-      contact.contactAddress = email;
-      contact.contactType = 'email';
+      contact.contactAddress = email
+      contact.contactType = 'email'
       contactGroups.forEach((contactGroup) => {
         if (contactGroup.Emails.indexOf(email > -1)) {
           contact.foreignContactGroups.push({
@@ -37,8 +42,8 @@ module.exports = {
           })
         }
       })
-      contacts.push(contact);
+      contacts.push(contact)
     })
-    return contacts;
+    return contacts
   }
-};
+}
