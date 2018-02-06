d54ee0eac6ba9480c8cf525b15338e1723f243e6
Larry Landon <larryl011010@gmail.com>
2018-02-05 20:04:33 -0600

    refactor contact/group mapping to allow easier mapping to tests

--- clients/NP_utils.js
+++ clients/NP_utils.js
@@ -1,15 +1,22 @@
-const rp = require('request-promise');
-const Promise = require('bluebird');
-const credentials = require('../credentials.js');
-const _ = require('lodash');
-const apiBaseUrl = 'https://api.nodeping.com/api/1/';
+const rp = require('request-promise')
+const Promise = require('bluebird')
+const credentials = require('../credentials.js')
+const _ = require('lodash')
+const apiBaseUrl = 'https://api.nodeping.com/api/1/'
 
-npCredentials = credentials.nodePing;
+npCredentials = credentials.nodePing
 var options = {
   method: 'GET',
   uri: `${apiBaseUrl}contacts/?token=${npCredentials.token}`,
   json: true
-};
+}
+
+const createContactGroup = (groupInfo) => {
+  options.uri = `${apiBaseUrl}contactgroups/?token=${npCredentials.token}`
+  options.method = 'POST'
+  options.body = groupInfo
+  return rp(options)
+}
 
 module.exports = {
   getNpContacts: function() {
@@ -18,16 +25,40 @@ module.exports = {
   //map foreign contacts to existing NP contacts, match on email
   mapContacts: function(NpContacts, foreignContacts) {
     return Promise.map(foreignContacts, (foreignContact) => {
-      contact = _.cloneDeep(foreignContact);
+      contact = _.cloneDeep(foreignContact)
 
       for (NpContactID in NpContacts) {
         NpContact = NpContacts[NpContactID]
         if (NpContact.name === foreignContact.contactAddress){
-          contact.NpContact = NpContact;
+          contact.NpContact = NpContact
         }
       }
       return contact
     })
+  },
+  mapContactsToGroups: function(contactMap) {
+    invertedGroupMap = {}
+
+    Promise.map(contactMap, (contact) => {
+
+      foreignGroups = contact.foreignContactGroups
+      foreignGroups.forEach((group) => {
+        mappedGroups = Object.keys(invertedGroupMap)
+        address = Object.keys(contact.NpContact.addresses)[0]
+        groupName = group.groupName
+        if (mappedGroups.indexOf(groupName) === -1){
+          invertedGroupMap[groupName] = {
+            foreignID: group.groupID,
+            addresses: [address]
+          }
+        } else {
+          invertedGroupMap[groupName].addresses.push(address)
+        }
+      })
+      return invertedGroupMap
+
+    })
+    console.log(invertedGroupMap)
   },
   createContact: function(contactInfo) {
     payload = {
@@ -41,12 +72,6 @@ module.exports = {
     }
     options.method = 'POST'
     options.body = payload
-    return rp(options)
-  },
-  createContactGroup: function(groupInfo) {
-    options.uri = `${apiBaseUrl}contactgroups/?token=${npCredentials.token}`
-    options.method = 'POST'
-    options.body = groupInfo
     return rp(options)
   }
 }
