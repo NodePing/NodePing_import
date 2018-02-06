343494b32a2fd79ca0d18b55b6996f727383a546
Larry Landon <larryl011010@gmail.com>
2018-02-05 20:07:58 -0600

    refactor to expect single complete map from foreign client and pass to NP client

--- sync-client.js
+++ sync-client.js
@@ -1,34 +1,29 @@
-const rp = require('request-promise');
+const rp = require('request-promise')
 const npClient = require('./clients/nodePingClient.js')
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2))
 
 //temporary hard-coded credentials
-const devCredentials = require('./credentials.js');
+const devCredentials = require('./credentials.js')
 
 //temporary source of credentials - convert to command line later
-const npCredentials = devCredentials.nodePing;
-const source = argv.s;
-const validSourceSites = ['statusCake'];
+const npCredentials = devCredentials.nodePing
+const source = argv.s
+const validSourceSites = ['statusCake']
 
 if (validSourceSites.indexOf(source) === -1) {
-  console.log('Sorry, no client exists for that source!');
-  console.log(`Valid sources are ${validSourceSites}`);
-  console.log('Use the argument -s <sourceSite>');
-  process.exit();
+  console.log('Sorry, no client exists for that source!')
+  console.log(`Valid sources are ${validSourceSites}`)
+  console.log('Use the argument -s <sourceSite>')
+  process.exit()
 }
 
 //selectively create required egressClient
-egressClient = require(`./clients/${source}EgressClient`);
+egressClient = require(`./clients/${source}EgressClient`)
 
 //temporary source of credentials - convert to command line later
-egressCredentials = devCredentials[source];
+egressCredentials = devCredentials[source]
 
-egressClient.getContactGroups(egressCredentials)
-.then((contacts) => {
-  npClient.syncContacts(contacts, npCredentials);
-})
-
-egressClient.getTests(egressCredentials)
-.then((tests) => {
-  npClient.syncTests(tests, npCredentials);
+egressClient.getDataMap(egressCredentials)
+.then((dataMap) => {
+  npClient.sync(dataMap, npCredentials)
 })
