8a71c1ae36ca6ff4f887e02ae9df222088d24328
Larry Landon <larryl011010@gmail.com>
2018-02-05 20:07:40 -0600

    refactor statusCakeClient to return single complete map of contacts/groups/tests

--- clients/statusCakeEgressClient.js
+++ clients/statusCakeEgressClient.js
@@ -1,41 +1,50 @@
-const rp = require('request-promise');
-const Promise = require('bluebird');
-const _ = require('lodash');
-const utils = require('./SC_utils.js');
+const rp = require('request-promise')
+const Promise = require('bluebird')
+const utils = require('./SC_utils.js')
 
-const apiBaseUrl = 'https://app.statuscake.com/API/';
+const apiBaseUrl = 'https://app.statuscake.com/API/'
+
+const getTests = (credentials) => {
+  const tests = []
+  let options = {
+    uri: `${apiBaseUrl}Tests/`,
+    headers: {
+        'API': credentials.token,
+        'Username': credentials.user
+    },
+    json: true
+  }
+  return rp(options)
+}
+
+const getContactGroups = (credentials) => {
+  let options = {
+    uri: `${apiBaseUrl}ContactGroups/`,
+    headers: {
+        'API': credentials.token,
+        'Username': credentials.user
+    },
+    json: true
+  }
+  return rp(options)
+  .then((results) => {
+    return utils.mapContactsAndGroups(results)
+  })
+}
 
 module.exports = {
-  getTests: function(credentials) {
-    const tests = [];
-    var options = {
-      uri: `${apiBaseUrl}Tests/`,
-      headers: {
-          'API': credentials.token,
-          'Username': credentials.user
-      },
-      json: true
-    };
-    return rp(options)
-    .then((results) => {
-      results.forEach((result) => {
-        tests.push(utils.mapTestToCheck(result));
-      });
-      return tests
-    })
-  },
-  getContactGroups: function(credentials) {
-    var options = {
-      uri: `${apiBaseUrl}ContactGroups/`,
-      headers: {
-          'API': credentials.token,
-          'Username': credentials.user
-      },
-      json: true
-    };
-    return rp(options)
-    .then((results) => {
-      return utils.mapContactsAndGroups(results);
+  getDataMap: function(credentials) {
+    return getTests(credentials)
+    .then((tests) => {
+      const checks = utils.mapTestsToChecks(tests)
+      return getContactGroups(credentials)
+      .then((contactMap) => {
+        dataMap = {
+          contactMap: contactMap,
+          checks: checks
+        }
+        return dataMap
+      })
     })
   }
 }
