f57bdf3648fb9e5e2ffc45a86ca880f06aec2321
Larry Landon <larryl011010@gmail.com>
2018-02-22 18:59:52 -0600

    get and write contacts and tests

--- clients/statuscake/statuscakeExporter.js
+++ clients/statuscake/statuscakeExporter.js
@@ -1,5 +1,7 @@
 const rp = require('request-promise')
 const Promise = require('bluebird')
+const csvWriter = require('csv-write-stream')
+const fs = require('fs')
 
 const apiBaseUrl = 'https://app.statuscake.com/API'
 
@@ -11,7 +13,23 @@ const options = {
 
 const getContactGroups = (credentials) => {
   options.uri = `${apiBaseUrl}/ContactGroups`
+  options.headers = {
+      'API': credentials.token,
+      'Username': credentials.user
+  }
   return rp(options)
+}
+
+const writeContacts = (data) => {
+  const rows = []
+  let columnNames
+  let values
+  data.forEach((contact) => {
+    columnNames = Object.keys(contact)
+    values = Object.values(contact)
+    rows.push(values)
+  })
+  write(columnNames, rows, 'contacts')
 }
 
 const getMaintenanceWindows = (credentials) => {
@@ -49,15 +67,38 @@ const getAllTests = (credentials) => {
   return rp(options)
 }
 
-const getTestDetails = (credentials, testID) => {
-  options.uri = `${apiBaseUrl}/Details/?TestID=${testID}`
-  return rp(options)
+const writeTests = (data) => {
+  const rows = []
+  let columnNames
+  let values
+  data.forEach((test) => {
+    columnNames = Object.keys(test)
+    values = Object.values(test)
+    rows.push(values)
+  })
+  write(columnNames, rows, 'testMetas')
+}
+
+const write = (columnNames, rows, entityName) => {
+  var writer = csvWriter({ headers: columnNames})
+  writer.pipe(fs.createWriteStream(`${entityName}.csv`))
+  rows.forEach((row) => {
+    writer.write(row)
+  })
+  writer.end()
 }
 
 
-
 module.exports = {
-  exportAllData: function(credentials) {
+  export: function(credentials) {
+    getContactGroups(credentials)
+    .then((data) => {
+      writeContacts(data)
+    })
+    getAllTests(credentials)
+    .then((data) => {
+      writeTests(data)
+    })
     
   }
 }
