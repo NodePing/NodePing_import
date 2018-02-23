c48fabbec15a139b7698d9dda4e97e565f12051c
Larry Landon <larryl011010@gmail.com>
2018-02-22 21:39:40 -0600

    refactor statuscake egress client

--- clients/statuscake/statuscakeEgressClient.js
+++ clients/statuscake/statuscakeEgressClient.js
@@ -4,28 +4,17 @@ const utils = require('./SC_utils.js')
 
 const apiBaseUrl = 'https://app.statuscake.com/API/'
 
-const getTests = (credentials) => {
-  const tests = []
-  let options = {
-    uri: `${apiBaseUrl}Tests/`,
-    headers: {
-        'API': credentials.token,
-        'Username': credentials.user
-    },
-    json: true
-  }
+const options = {
+  json: true
+}
+
+const getTests = () => {
+  options.uri = `${apiBaseUrl}Tests/`
   return rp(options)
 }
 
 const getContactGroups = (credentials) => {
-  let options = {
-    uri: `${apiBaseUrl}ContactGroups/`,
-    headers: {
-        'API': credentials.token,
-        'Username': credentials.user
-    },
-    json: true
-  }
+  options.uri = `${apiBaseUrl}ContactGroups/`
   return rp(options)
   .then((results) => {
     return utils.mapContactsAndGroups(results)
@@ -34,10 +23,14 @@ const getContactGroups = (credentials) => {
 
 module.exports = {
   getDataMap: function(credentials) {
-    return getTests(credentials)
+    options.headers = {
+          'API': credentials.token,
+          'Username': credentials.user
+    }
+    return getTests()
     .then((tests) => {
       const checks = utils.mapTestsToChecks(tests)
-      return getContactGroups(credentials)
+      return getContactGroups()
       .then((contactMap) => {
         dataMap = {
           contactMap: contactMap,
